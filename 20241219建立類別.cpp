339275debaabc7bf7c9621d949c362fcf20b94e2
kuma-lioho <131100987+kuma-lioho@users.noreply.github.com>
2024-12-19 15:19:29 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -8,6 +8,17 @@ class myTime{
 		void printTime();
 };
 
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
 int main(){
-
+	myTime now, open;
+	now.hour = 8;
+	now.minute = 18;
+	cout<<"現在時間:";
+	now.printTime();
+	open.hour = 9;
+	open.minute = 10;
+	cout<<"開張時間:";
+	open.printTime(); 
 }
