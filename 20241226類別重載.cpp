051cb07988f61393933a40991dab846cf3737b10
kuma-lioho <131100987+kuma-lioho@users.noreply.github.com>
2024-12-26 14:45:02 +0800

    Update 20241226類別重載.cpp

--- 20241226類別重載.cpp
+++ 20241226類別重載.cpp
@@ -10,22 +10,28 @@ class myTime{
 		void setTime(int h ,int m);
 		void printTime();
 };
-
 myTime::myTime(){
 	hour = 9; minute = 0;
 }
 myTime::myTime(int h, int m){
 	setTime(h, m);
 }
-void setTime(int h, int m){
+void myTime::setTime(int h, int m){
 	hour = h;
 	minute = m;
 }
-void printTime(){
+void myTime::printTime(){
 	cout<<hour<<":"<<minute<<"\n";
 }
-
 main(){
-	
+	myTime open;
+	myTime close(21);
+	myTime close1(21,25);
+	cout<<"開張時間:";
+	open.printTime();
+	cout<<"結束時間(1):";
+	close.printTime();
+	cout<<"結束時間(2):";
+	close1.printTime();
 }
 
